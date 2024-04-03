0e7b6a79f1b7f3c2d5a3e9c19e5d20a4df3d4bb5
Przemek <przemek.lewtak@heywood.co.uk>
2024-04-03 22:49:34 +0100

    Added Suspense to auth pages

--- app/(application)/(auth)/layout.tsx
+++ app/(application)/(auth)/layout.tsx
@@ -1,12 +1,14 @@
+import { Suspense } from "react";
 import { Navbar } from "@/app/components/ui/Navbar";
 import { FooterAuth } from "@/app/components/ui/FooterAuth/FooterAuth";
+import Loading from "@/app/loading";
 
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<>
 			<Navbar />
 			<main className="animated flex min-h-[50vh] max-w-full flex-2 flex-col bg-foreground-grey pt-[80px]">
-				{children}
+				<Suspense fallback={<Loading />}>{children}</Suspense>
 			</main>
 			<FooterAuth />
 		</>
