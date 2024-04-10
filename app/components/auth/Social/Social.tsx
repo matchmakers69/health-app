1898901a5004a60ca4b0154c89c8a0d4fe9be726
Przemek <przemek.lewtak@heywood.co.uk>
2024-04-10 14:33:05 +0100

    Updated auth.config - added providers

--- app/components/auth/Social/Social.tsx
+++ app/components/auth/Social/Social.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 import { signIn } from "next-auth/react";
-import { useSearchParams } from "next/navigation";
 import { Button } from "../../ui/Button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 export const Social = () => {
-	const searchParams = useSearchParams();
-	const callbackUrl = searchParams.get("callbackUrl");
-
 	const onClick = (provider: "google" | "github") => {
 		signIn(provider, {
-			callbackUrl: callbackUrl ?? "/",
+			callbackUrl: DEFAULT_LOGIN_REDIRECT,
 		});
 	};
 
