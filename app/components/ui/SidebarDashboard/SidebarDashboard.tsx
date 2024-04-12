ed545859b06a6cd7c5d04ac6e69c9bab9c75f6e9
Przemek <przemek.lewtak@heywood.co.uk>
2024-04-12 15:19:42 +0100

    Added changes to sign out button for dashboard sidebar

--- app/components/ui/SidebarDashboard/SidebarDashboard.tsx
+++ app/components/ui/SidebarDashboard/SidebarDashboard.tsx
@@ -1,19 +1,25 @@
 import { Button } from "../Button";
 import { Sidebar } from "./Sidebar";
+import { appLinkLabels } from "@/lib/appData";
 import { signOut } from "@/auth";
 
 function SidebarDashboard() {
 	return (
 		<Sidebar>
-			<div className="sidebar-user-section border-footer-nav py-[1.5rem]">
+			<div className="sidebar-user-section border-footer-nav">
 				<form
 					action={async () => {
 						"use server";
 						await signOut();
 					}}
 				>
-					<Button className="w-full bg-primary-foreground uppercase text-navy" variant="default" size="sm">
-						Logout
+					<Button
+						className="h-full w-full items-center justify-start px-0 py-[1.2rem] text-md text-text-light"
+						variant="ghost"
+						type="submit"
+					>
+						<i className="ri-logout-box-line mr-4 block" />
+						{appLinkLabels.SIGN_OUT}
 					</Button>
 				</form>
 			</div>
