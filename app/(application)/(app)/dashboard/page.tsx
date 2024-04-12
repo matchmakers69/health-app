382d19323d941b65102bd09d068cc7f0e1bc7ab6
Przemek <przemek.lewtak@heywood.co.uk>
2024-04-12 11:31:57 +0100

    Updated Sidebar for dashboard

--- app/(application)/(app)/dashboard/page.tsx
+++ app/(application)/(app)/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import { DashboardContent } from "@/app/components/dashboard/DashboardContent";
-import { Button } from "@/app/components/ui/Button";
-import { auth, signOut } from "@/auth";
-import { appLinkLabels } from "@/lib/appData";
+import { auth } from "@/auth";
 
 const Dashboard = async () => {
 	const session = await auth();
@@ -9,21 +7,7 @@ const Dashboard = async () => {
 		<div className="text-dark-green">
 			<h3>Dashboard</h3>
 			{JSON.stringify(session)}
-			<form
-				className="flex h-full w-full flex-col justify-center"
-				action={async () => {
-					"use server";
-					await signOut();
-				}}
-			>
-				<Button
-					className="flex h-full w-full flex-col items-start  justify-center px-[1.2rem] py-0 text-[1.6rem] font-normal text-navy"
-					variant="ghost"
-					type="submit"
-				>
-					{appLinkLabels.SIGN_OUT}
-				</Button>
-			</form>
+
 			<DashboardContent />
 		</div>
 	);
---
+++ app/components/ui/SidebarDashboard/Sidebar/Sidebar.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import Link from "next/link";
+import { CloseSidebarButton } from "../../CloseSidebarButton/CloseSidebarButton";
+import { Logo } from "../../Logo";
+import { type SidebarProps } from "./defs";
+import { routes } from "@/lib/routes";
+import { useDashboardLayoutContext } from "@/app/context/DashboardLayoutContext";
+
+function Sidebar({ children }: SidebarProps) {
+	const { isSidebarInView, dispatch } = useDashboardLayoutContext();
+
+	const handleCloseSidebar = () => {
+		dispatch({
+			type: "TOGGLE_SIDEBAR",
+			payload: !isSidebarInView,
+		});
+	};
+	return (
+		<aside
+			className={`${!isSidebarInView ? "-translate-x-full" : "translate-x-0"} sidebar-menu fixed left-0 top-0 z-50 flex h-full w-[26rem] flex-shrink-0 flex-col transition-transform`}
+		>
+			<div className="sidebar-inner flex h-full min-h-0 flex-1 flex-col bg-foreground-green px-[1.5rem] md:px-[2rem]">
+				<div className="sidebar-content flex flex-1 flex-col overflow-y-auto pb-4">
+					<header className="sidebar-header flex flex-col gap-6 pt-[2rem]">
+						<div className="md:hidden">
+							<CloseSidebarButton onClose={handleCloseSidebar} />
+						</div>
+						<div className="site-header-logo border-bottom-nav relative mb-[2rem] pb-[2rem]">
+							<Link className="flex flex-col items-center justify-center" href={routes.HOME}>
+								<Logo width={45} height={45} />
+							</Link>
+						</div>
+					</header>
+				</div>
+
+				{children}
+			</div>
+		</aside>
+	);
+}
+
+export { Sidebar };
