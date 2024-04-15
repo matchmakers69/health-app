21f84a1da9261b5f72569efc295ef915a8b3a933
Przemek <przemek.lewtak@heywood.co.uk>
2024-04-15 14:50:16 +0100

    Fixed logout from dropdown dashboard

--- components/ui/DropdownNavbarList/DropdownNavbarList.tsx
+++ components/ui/DropdownNavbarList/DropdownNavbarList.tsx
@@ -1,13 +1,14 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "../Button";
 import { type DropdownNavbarListProps } from "./defs";
 import { appLinkLabels } from "@/lib/appData";
-import { signOut } from "@/auth";
-import { routes } from "@/lib/routes";
+import { logout } from "@/actions/signout";
 
 function DropdownNavbarList({ dropDownList }: DropdownNavbarListProps) {
 	const handleSignOut = async () => {
-		await signOut({ redirectTo: routes.LOGIN });
+		await logout();
 	};
 	return (
 		<ul className="drop-down-list w-full">
@@ -18,21 +19,14 @@ function DropdownNavbarList({ dropDownList }: DropdownNavbarListProps) {
 							key={dropDownLink.id}
 							className="h-[4.5rem] w-full transition-all duration-300 hover:rounded-2xl hover:bg-[rgba(31,81,156,.2)]"
 						>
-							<form
-								className="flex h-full w-full flex-col justify-center"
-								action={async () => {
-									"use server";
-									await signOut();
-								}}
+							<Button
+								className="flex h-full w-full flex-col items-start  justify-center px-[1.2rem] py-0 text-[1.6rem] font-normal text-navy"
+								variant="ghost"
+								onClick={handleSignOut}
+								type="button"
 							>
-								<Button
-									className="flex h-full w-full flex-col items-start  justify-center px-[1.2rem] py-0 text-[1.6rem] font-normal text-navy"
-									variant="ghost"
-									type="submit"
-								>
-									{appLinkLabels.SIGN_OUT}
-								</Button>
-							</form>
+								{appLinkLabels.SIGN_OUT}
+							</Button>
 						</li>
 					);
 				}
